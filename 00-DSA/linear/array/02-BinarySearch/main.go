369957eb4abb8f2dadea1822309d7747f11d6938
ArjunDev17 <arjun.devb25@gmail.com>
2024-09-02 01:14:14 +0530

    more feature

---
+++ 00-DSA/linear/array/02-BinarySearch/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func binarySearch(arr []int, target int) int {
+	start := 0
+	end := len(arr) - 1
+	for start < end {
+		mid := (start + end) / 2
+		if arr[mid] == target {
+
+		}
+	}
+	return -1
+}
+func main() {
+	fmt.Println("Hello")
+}
