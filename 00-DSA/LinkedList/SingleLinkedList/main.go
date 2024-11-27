855eae27ee2f77e051a5a58413ffb234cd391f0c
arjunsdev17 <arjun.singh@razorpay.com>
2024-11-27 11:24:48 +0530

    error handling

---
+++ 00-DSA/LinkedList/SingleLinkedList/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+type Node struct {
+	Data string
+	Next *Node
+}
+
+func insertFirstNode(data string) {
+	head := &Node{}
+	fmt.Println("head :", head)
+
+}
+
+/*
+insert first node
+delete first node
+traversal node
+*/
+
+func main() {
+	fmt.Println("Linked List")
+}
